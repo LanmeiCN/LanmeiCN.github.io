91cd268a4c698eb6a9daede5e6b552f9bb1c0b9c
蓝莓莓 <83525433+LanmeiCN@users.noreply.github.com>
2023-02-04 22:42:11 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="nofollow"/>
+
+    <!-- MDUI CSS -->
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/css/mdui.min.css"
+    integrity="sha384-cLRrMq39HOZdvE0j6yBojO4+1PrHfB7a9l5qLcmRm/fiWXYY+CndJPmyu5FV/9Tw"
+    crossorigin="anonymous"
+    />
+
+    <!-- MDUI JavaScript -->
+    <script
+    src="https://cdn.jsdelivr.net/npm/mdui@1.0.1/dist/js/mdui.min.js"
+    integrity="sha384-gCMZcshYKOGRX9r6wbDrvF+TcCCswSHFucUzUPwka+Gr+uHgjlYvkABr95TCOz3A"
+    crossorigin="anonymous"
+    ></script>
+
+    <link rel="stylesheet" href="./assets/namie.css" />
+
+    <meta name="description" content="Example Description">
+    <meta name="keywords" content="minecraft, server, mc, minecraft server, Example Server">
+    <meta name="author" content="Example.xyz">
+    <link rel="icon" href="assets/logo.ico"> 
+
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://www.Example.xyz">
+    <meta property="og:title" content="Example Server">
+    <meta property="og:description" content="Example Description">
+    <meta property="og:image" content="https://image">
+    <meta name="theme-color" content=#12345>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" integrity="sha512-c42qTSw/wPZ3/5LBzD+Bw5f7bSF2oxou6wEb+I/lqeaKV5FDIfMvvRp772y4jcJLKuGUOpbJMdg/BTl50fJYAw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="dist/style.css"> 
+
+</head>
+<body>
+    <!--- Particles -->  <div class="animation-wrapper"> <div class="particle particle-1"></div> <div class="particle particle-2"></div> <div class="particle particle-3"></div> <div class="particle particle-4"></div> </div>
+    <div class="center" style="top: 0% !important; transform: translate(-50%, 0%) !important;">
+        <div class="animate__animated animate__jackInTheBox">
+            <h1>莓利坚</h1> <!--- Server Name -->
+            <p>成分复杂到无法评价的qq游戏开黑/交流/闲聊群</p> <!--- Description -->
+            <p>两大up主入驻：（单击查看详情）</p> <!--- Description -->
+            <div class="mdui-container">
+                <div class="mdui-tab mdui-col-xs-12 mdui-tab-full-width" mdui-tab>
+                    <a href="#tab1" class="mdui-ripple mdui-tab-active">阿宅</a>
+                    <a href="#tab2" class="mdui-ripple">CubeWhy</a>
+                </div>
+                <div id="tab1" class="mdui-p-a-2">
+                    <div>
+                        <img src="https://i0.hdslb.com/bfs/face/40b13bebb8bb50bedf86538e5d05fd79e72f541f.jpg" style="width: 30%;height: auto;border-radius: 50%;margin-top: 5%;"><br/>
+                        <div class="mdui-card-primary-title">阿宅</div>
+                        <div class="mdui-card-primary-subtitle">原神/Minecraft区Up主<br/>全网粉丝115.1w</div>
+                        <button class="mdui-btn mdui-ripple mdui-btn-dense mdui-color-white mdui-btn-icon mdui-shadow-17" style="margin-top: -10%;" onclick="window.location.href=`https://space.bilibili.com/382299222`"><img src="https://space.bilibili.com/favicon.ico" style="width: 75%; position: relative;left: 0.6%;top: 10%;"></button>
+                        <button class="mdui-btn mdui-ripple mdui-btn-dense mdui-color-white mdui-btn-icon mdui-shadow-17" style="margin-top: -10%;" onclick="window.location.href=`https://www.ixigua.com/home/1402593660113710/`"><img src="https://www.ixigua.com/favicon.ico" style="transform: translateY(2%);"></button>
+                    </div>
+                </div>
+                <div id="tab2" class="mdui-p-a-2">
+                    <div>
+                        <img src="https://i0.hdslb.com/bfs/face/bde69c9fdbcf4bdbb6a52aa5f86633f9d18f5dc0.jpg" style="width: 30%;height: auto;border-radius: 50%;margin-top: 5%;"><br/>
+                        <div class="mdui-card-primary-title">CubeWhy</div>
+                        <div class="mdui-card-primary-subtitle">Minecraft区Up主<br/>LunarClient-CN客户端作者<br/>全网粉丝2.6k</div>
+                        <button class="mdui-btn mdui-ripple mdui-btn-dense mdui-color-white mdui-btn-icon mdui-shadow-17" style="margin-top: -10%;" onclick="window.location.href=`https://space.bilibili.com/1106744676`"><img src="https://space.bilibili.com/favicon.ico" style="width: 75%; position: relative;left: 0.6%;top: 10%;"></button>
+                    </div>
+                </div>
+            </div>
+            <p style="font-weight: 500;font-size: 45%;">全新政策</p><p style="font-weight: 300;font-size: 32%;">群中启用了机器人，将不定时（每个月至少一次）开启活跃探测（不会通知），在启动的时间段内发言次数前十名获得管理员。Up主可常驻管理员。<br/></p>
+            <p>群中常玩:</p>
+            <div class="mdui-container">
+                <div class="mdui-col-xs-6"><p style="font-weight: 500;font-size: 38%;">GTA5在线模式</p><p style="font-weight: 300;font-size: 27%;">GTA:OL<br/>常联机</p></div>
+                <div class="mdui-col-xs-6"><p style="font-weight: 500;font-size: 38%;">求生之路2</p><p style="font-weight: 300;font-size: 27%;">Life 4 Dead 2<br/>常联机</p></div>
+                <div class="mdui-col-xs-6"><p style="font-weight: 500;font-size: 38%;">都市:天际线</p><p style="font-weight: 300;font-size: 27%;">Cities:Skyline<br/>群中免费解锁DLC</p></div>
+                <div class="mdui-col-xs-6"><p style="font-weight: 500;font-size: 38%;">原神</p><p style="font-weight: 300;font-size: 27%;">Genshin Impact<br/>群中有原神机器人</p></div>
+            </div>
+            <div class="animate__animated animate__bounceIn animate__delay-1s">
+                <a href="https://jq.qq.com/?_wv=1027&k=2wXkCFz4"><button class="btn1"><i class="fab fa-shopping-basket"></i> 现在加入！</button></a>
+            </div>
+        </div>
+    </div>
+    <script src="app/js/script.js"></script>
+    <script src="https://kit.fontawesome.com/5acf4d9e80.js" crossorigin="anonymous"></script>
+</body>
+</html>
